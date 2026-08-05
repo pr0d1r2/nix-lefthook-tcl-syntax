b12187432909ddb68ee5c7cf6a0e6552809ca80a
Marcin Nowicki <pr0d1r2@gmail.com>
2026-08-05 16:53:19 +0200

    chore: migrate vendored → referenced (set-and-setting standard) (#30)
    Co-authored-by: hallucinogen <hallucinogen@localhost>

--- flake.nix
+++ flake.nix
@@ -1,13 +1,36 @@
 {
   description = "CHANGEME";
+
   nixConfig = {
     extra-substituters = [ "https://pr0d1r2.cachix.org" ];
     extra-trusted-public-keys = [ "pr0d1r2.cachix.org-1:NfWjbhgAj41byXhCKiaE+av3Vnphm1fTezHXEGsiQIM=" ];
   };
+
   inputs = {
     nixpkgs-lock.url = "github:pr0d1r2/nixpkgs-lock";
     nixpkgs.follows = "nixpkgs-lock/nixpkgs";
+
     set-and-setting.url = "github:pr0d1r2/set-and-setting";
+    set-and-setting.inputs.nixpkgs-lock.follows = "nixpkgs-lock";
   };
-  outputs = inputs: (import ./flake-outputs.nix).outputs inputs;
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      set-and-setting,
+      ...
+    }:
+    set-and-setting.lib.mkConsumerFlake {
+      inherit self nixpkgs set-and-setting;
+      fragments = [
+        "base"
+        "nix"
+        "shell"
+        "ascii"
+        "markdown"
+        "yaml"
+      ];
+      src = ./.;
+    };
 }
