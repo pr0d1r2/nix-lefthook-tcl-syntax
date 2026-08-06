ff83770dec0a47062ee50919e953ec19e79ce10a
hallucinogen <hallucinogen@localhost>
2026-08-06 15:08:55 +0000

    chore: migrate vendored → referenced (set-and-setting standard)

--- flake.nix
+++ flake.nix
@@ -39,12 +39,12 @@
           scaffold = set-and-setting.lib.mkConsumerFlake {
             inherit self nixpkgs set-and-setting;
             fragments = [
-              "base"
-              "nix"
-              "shell"
-              "ascii"
-              "markdown"
-              "yaml"
+        "base"
+        "nix"
+        "shell"
+        "ascii"
+        "markdown"
+        "yaml"
             ];
             src = ./.;
           };
